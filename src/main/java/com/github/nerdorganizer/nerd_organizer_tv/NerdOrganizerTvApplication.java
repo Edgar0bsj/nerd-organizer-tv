223e296a34c695778e2ae4204cdae2a40f6c936d
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-04-05 09:17:11 -0300

    starting do projeto

---
+++ src/main/java/com/github/nerdorganizer/nerd_organizer_tv/NerdOrganizerTvApplication.java
@@ -0,0 +1,13 @@
+package com.github.nerdorganizer.nerd_organizer_tv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdOrganizerTvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdOrganizerTvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nerdorganizer/nerd_organizer_tv/NerdOrganizerTvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nerdorganizer.nerd_organizer_tv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdOrganizerTvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
